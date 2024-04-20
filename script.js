f810fe94d4c3b6af7b21ee8eeaa0a34836aee69f
RomualdAv <RomualdAv>
2024-04-20 17:37:13 +0200

    Section cartes V2

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const sliderContainer = document.getElementById("sliderContainer");
+    const scrollLeftButton = document.getElementById("scrollLeftButton");
+    const scrollRightButton = document.getElementById("scrollRightButton");
+    
+    const scrollStep = 400;
+    
+    scrollLeftButton.addEventListener("click", function() {
+        sliderContainer.scrollBy({
+            left: -scrollStep,
+            behavior: "smooth"
+        });
+    });
+    
+    scrollRightButton.addEventListener("click", function() {
+        sliderContainer.scrollBy({
+            left: scrollStep,
+            behavior: "smooth"
+        });
+    });
+});
